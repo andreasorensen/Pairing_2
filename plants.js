af366f47035f6d57a62b0490d01b7965a8d7d525
Andrea Sorensen <dreasor2987@icloud.com>
2023-03-01 15:43:40 -0800

    Create if statement

---
+++ plants.js
@@ -0,0 +1,7 @@
+var gets_sunshine = true;
+var ounces_of_water = 20;
+if (gets_sunshine = true && ounces_of_water >= 10){
+    console.log("Your plant will thrive")
+} else {
+    console.log("This plant will die!")
+}
